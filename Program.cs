e015c181b61ed62dbf55e48aacb08e94677a68a1
Alanna Smith <smitalan240@gmail.com>
2024-03-07 14:42:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace hangman_topic_8._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            static void hangmanOne()
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n      |\r\n      |\r\n      |\r\n      |\r\n=========");
+            }
+            static void hangmanTwo()
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n      |\r\n      |\r\n      |\r\n=========");
+            }
+            static void hangmanThree()
+            {
+
+            }
+        }
+    }
+}
